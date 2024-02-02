086950034b3a8ff9acce62b8b1506fc2579daa5f
danagur1 <danagur1@mail.tau.ac.il>
2024-02-03 00:52:58 +0200

    check just rules dev

--- firewall.c
+++ firewall.c
@@ -12,13 +12,14 @@ MODULE_AUTHOR("Dana Gur");
 MODULE_DESCRIPTION("Stateless firewall");
 
 static int __init my_module_init_function(void) {
-	return register_hook() && log_clear_create_dev() && log_show_create_dev() && rules_create_dev();
+	return rules_create_dev()/*register_hook() && log_clear_create_dev() && log_show_create_dev() && rules_create_dev();*/
 }
 
 static void __exit my_module_exit_function(void) {
+	/*
 	unregister_hook();
 	log_clear_remove_dev();
-	log_show_remove_dev();
+	log_show_remove_dev();*/
 	rules_remove_dev();
 }
 module_init(my_module_init_function);
