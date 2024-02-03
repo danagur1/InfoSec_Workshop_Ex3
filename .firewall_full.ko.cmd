7fecec8fdab4ee4706c7cdd6c58f1157dbaf83a2
danagur1 <danagur1@mail.tau.ac.il>
2024-02-03 14:58:36 +0200

    removed unneccery files

--- .firewall_full.ko.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/fw/Desktop/hw3/Infosec_Workshop_Ex3/firewall_full.ko := ld -r -m elf_i386 -T ./scripts/module-common.lds --build-id  -o /home/fw/Desktop/hw3/Infosec_Workshop_Ex3/firewall_full.ko /home/fw/Desktop/hw3/Infosec_Workshop_Ex3/firewall_full.o /home/fw/Desktop/hw3/Infosec_Workshop_Ex3/firewall_full.mod.o ;  true
--- .firewall_full.o.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/fw/Desktop/hw3/Infosec_Workshop_Ex3/firewall_full.o := ld -m elf_i386   -r -o /home/fw/Desktop/hw3/Infosec_Workshop_Ex3/firewall_full.o /home/fw/Desktop/hw3/Infosec_Workshop_Ex3/firewall.o /home/fw/Desktop/hw3/Infosec_Workshop_Ex3/rules_functions.o 
