a3b02bc5d320355ca297f3fbb6e32db3ee3ce456
danagur1 <danagur1@mail.tau.ac.il>
2024-02-05 16:59:44 +0200

    check another way of using klist

--- manage_log_list.c
+++ manage_log_list.c
@@ -32,7 +32,7 @@ void init_log_list(void) {
     klist_init(&log_list);
 }*/
 
-int insert_log(struct log_row_t *log) {
+int insert_log(log_row_t *log) {
     struct klist_node *node = kmalloc(sizeof(struct klist_node), GFP_KERNEL);
     if (!node) {
         return -1;
@@ -41,6 +41,47 @@ int insert_log(struct log_row_t *log) {
     node->n_klist = log;
     return 0;
 }
+
+void remove_all_from_log_list() {
+    struct klist_iter iter;
+    struct klist_node *node;
+    klist_iter_init(&log_list, &iter);
+    while ((node = klist_next(&iter)) != NULL) {
+        klist_del(node);
+        kfree(node);
+    } 
+    klist_iter_exit(&iter);
+}
+
+log_row_t *find_identical_log(log_row_t *log, int (*compare_logs)(log_row_t *log1, log_row_t *log2)) {
+    struct klist_iter iter;
+    struct klist_node *node;
+    klist_iter_init(&log_list, &iter);
+    while ((node = klist_next(&iter)) != NULL) {
+        if (compare_logs(node->n_klist, log) == 0) {
+            klist_iter_exit(&iter);
+            return node->n_klist;
+        }
+    }
+    klist_iter_exit(&iter);
+    return NULL;
+}
+
+int func_for_log_list(int (*func)(log_row_t)) {
+    struct klist_iter iter;
+    struct klist_node *node;
+    int result;
+    klist_iter_init(&log_list, &iter);
+    while ((node = klist_next(&iter)) != NULL) {
+        result= func(node->n_klist);
+        if (result==-1){
+            return -1;
+        }
+    }
+    klist_iter_exit(&iter);
+    return 0;
+}
+
 /*
 void remove_all_from_log_list(void) {
     struct log_list_node *entry;
