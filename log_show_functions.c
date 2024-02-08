570f3fcf75d7c9493f7a6aa59e87adbe4f4c5f20
danagur1 <danagur1@mail.tau.ac.il>
2024-02-09 01:12:49 +0200

    added log show functions kernel-mode part

--- log_show_functions.c
+++ log_show_functions.c
@@ -13,15 +13,35 @@ char* buffer_index;							// The moving index of the original buffer
 static int major_number;					// Major of the char device
 static struct class* devices_class = NULL;	// The device's class
 static struct device* log_device = NULL;	// The device's name
+static char *log_output = NULL;
+int position_in_log_output = 0;
 
+static void free_log_output(void){
+    if (log_output!=NULL){
+        kfree(log_output);
+    }
+}
+
+static int print_log(log_row_t *log){
+    log_output+position_in_log_output = kmalloc(sizeof(log_row_t), GFP_KERNEL);
+    if (log_output+position_in_log_output==NULL){
+        return -1;
+    }
+    memcpy(log_output+position_in_log_output, log, sizeof(log_row_t));
+    log_to_str(log_output+position_in_log_output, log);
+    position_in_log_output+=sizeof(log_row_t);
+    return 0;
+}
 
 /* Our custom open function  for file_operations --------------------- */
-static int my_open(struct inode *_inode, struct file *_file) { // Each time we open the device we initilize the changing variables ( so we will be able to read it again and again
+static int my_open(struct inode *_inode, struct file *_file) { 
 	return 0;
 }
 
 /* Our custom read function  for file_operations --------------------- */
 static ssize_t my_read(struct file *filp, char *buff, size_t length, loff_t *offp) {
+    free_log_output();
+    func_for_log_list(print_log);
 	return 0;
 }
 
@@ -53,4 +73,5 @@ void log_show_remove_dev(void) {
 	device_destroy(devices_class, MKDEV(major_number, MINOR_LOG_SHOW));
 	class_destroy(devices_class);
 	unregister_chrdev(major_number, DEVICE_NAME_SHOW_LOG);
+    free_log_output();
 }
