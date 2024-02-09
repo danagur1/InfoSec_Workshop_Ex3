f0d4c72effe1ef2459fe132f330b087242f495fd
danagur1 <danagur1@mail.tau.ac.il>
2024-02-10 01:02:49 +0200

    fixed synatx error

--- manage_log_list.c
+++ manage_log_list.c
@@ -14,7 +14,7 @@ printk(KERN_INFO "init of log list");
 int add_to_log_list(log_row_t *log) {
     struct klist_node *node;
     if (log_list_length<5){
-        node = &log_node_pool[log_node_count++]; 
+        node = &log_node_pool[log_list_length++]; 
     }
     else{
         node = kmalloc(sizeof(struct klist_node), GFP_KERNEL);
