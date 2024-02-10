a65e1042ddca3b79a20f28c44f8913d22eb6a3a6
danagur1 <danagur1@mail.tau.ac.il>
2024-02-10 15:34:38 +0200

    started parsing log functions and correct read of validation bit

--- log_functions.py
+++ log_functions.py
@@ -1,12 +1,15 @@
+import struct
 LOG_SHOW_FILEPATH= "/dev/fw_log"
 LOG_CLEAR_FILEPATH= "/sys/class/fw/log/reset"
 
+def parse_timestamp(bytes_timestamp):
+    timestamp = struct.unpack(">IQ", bytes_timestamp)
 
 def load():
     try:
         with open(LOG_SHOW_FILEPATH, "rb") as log_show_file:
             #test:
-            validation_bit = log_show_file.read(1)
+            validation_bit = log_show_file.read(1)[0]
             while validation_bit==1:
                 print(log_show_file.read(13))
                 validation_bit = log_show_file.read(1)
