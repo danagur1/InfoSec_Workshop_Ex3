58e1a5b4012f5d8fb4009379412d0783df805b1a
danagur1 <danagur1@mail.tau.ac.il>
2024-02-11 08:52:44 +0200

    rule direction any should be checked seperatly

--- hooking_functions.c
+++ hooking_functions.c
@@ -25,14 +25,16 @@ int check_direction(struct sk_buff *skb, rule_t rule){
 	struct net_device *dev = skb->dev;
 	if (dev) {
 		printk(KERN_INFO "in check_direction: dev->name=%s\n", dev->name);
-        if ((rule.direction==DIRECTION_IN) || (rule.direction==DIRECTION_ANY)) {
+        if (rule.direction==DIRECTION_IN) {
             return strcmp(dev->name, "enp0s9") == 0;
-        } else if ((rule.direction==DIRECTION_OUT) || (rule.direction=DIRECTION_ANY)) {
+        } else if (rule.direction==DIRECTION_OUT) {
             return strcmp(dev->name, "enp0s8") == 0;
-        } else {
-			printk(KERN_INFO "in check_direction: no dev\n");
-            return -1;
+        } else if (rule.direction==DIRECTION_ANY){
+            return (strcmp(dev->name, "enp0s8") == 0) || (strcmp(dev->name, "enp0s9") == 0);
     	}
+		else{
+			return -1;
+		}
 	}
 	return 0;
 }
