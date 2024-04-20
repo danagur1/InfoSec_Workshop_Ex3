b311d7770d190318e4e4b672257f09901f50da7e
danagur1 <danagur1@mail.tau.ac.il>
2024-04-20 18:43:00 +0300

    better sending data between user mode and kernel mode format

--- rules_functions.c
+++ rules_functions.c
@@ -11,12 +11,19 @@ MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Dana Gur");
 MODULE_DESCRIPTION("Stateless firewall");
 
+const int RULE_FILEDS = 13;
+
+typedef int (*ParseFieldFuncPointer)(rule_t*);
+
 static int major_number;
 static struct class* fw_class = NULL;
 static struct device* rules = NULL;
 
 static rule_t *rule_table;
 static int *rule_table_size;
+
+const char *input_buf_pointer;
+int input_buf_index;
 
 static char *not_parsed_input = NULL;
 
@@ -55,79 +62,61 @@ static ssize_t display(struct device *dev, struct device_attribute *attr, char *
 Parse rule values from written to the driver:
 */
 
-static int parse_rule_name(const char *src, char *dst){
-	int size = 0;
-	while (src[size]!=' '){
-		size++;
-		if (size>20){
-			return -1;
-		}
+static int parse_rule_name(rule_t *curr_rule){
+	char *dst = curr_rule->rule_name;
+	if ((input_buf_pointer+input_buf_index)[20]!='\0'){
+		return -1; //error code
 	}
-	strncpy(dst, src, size);
-	dst[size] = '\0';
-	return size; //return the length of the parsed element 
+    memcpy(dst, input_buf_pointer+input_buf_index, 21*sizeof(char));
+	return 21*sizeof(char); //return the length of the parsed element 
 }
 
-static int parse_direction(const char *src, direction_t *dst){
-	*dst = src[0]-'0';
+static int parse_direction(rule_t *curr_rule){
+	direction_t *dst = &(curr_rule->direction);
+	*dst = (input_buf_pointer+input_buf_index)[0];
 	if ((0<=*dst)&&(*dst<=3)){
 		return 1; //return the length of the parsed element 
 	}
 	return -1; //error code
 }
 
-static int parse_ack(const char *src, ack_t *dst){
-	*dst = src[0]-'0';
-	if ((0<=*dst)&&(*dst<=3)){
-		return 1; //return the length of the parsed element 
-	}
-	return -1; //error code
+static int parse_ip(__be32 *dst){
+	memcpy(dst, input_buf_pointer+input_buf_index, sizeof(__be32));
+	return sizeof(__be32); //return the length of the parsed element
 }
 
-static int parse_ip(const char *src, __be32 *dst){
-	int size = 0;
-	struct in_addr addr;
-	while (src[size]!=' '){
-		size++;
-	}
-	if (in4_pton(src, size, (u8 *)&addr, -1, NULL)!=1){
-		return -1;
-	}
-	*dst = addr.s_addr;
-	return size;
+static int parse_src_ip(rule_t *curr_rule){
+	return parse_ip(&(curr_rule->src_ip));
 }
 
-static int parse_perfix_size(const char *src, __u8 *dst){
-	unsigned long src_long;
-	if (src[1] == ' '){
-		char perfix[2];
-		perfix[0] = src[0];
-		perfix[1] ='\0';
-		if (kstrtoul(perfix, 10, &src_long)!=0){
-			return -1;
-		}
-		*dst = (__u8)src_long;
-		return 1;
-	}
-	else if (src[2] == ' ')
-	{
-		char perfix[3];
-		perfix[0] = src[0];
-		perfix[1] = src[1];
-		perfix[2] ='\0';
-		if (kstrtoul(perfix, 10, &src_long)!=0){
-			return -1;
-		}
-		*dst = (__u8)src_long;
-		return 2;
-	}
-	else{
-		return -1;
-	}
+static int parse_src_prefix_mask(rule_t *curr_rule){
+	return parse_ip(&(curr_rule->src_prefix_mask));
 }
 
-static int parse_protocol(const char *src, __u8 *dst){
-	switch (src[0])
+static int parse_dst_ip(rule_t *curr_rule){
+	return parse_ip(&(curr_rule->dst_ip));
+}
+
+static int parse_dst_prefix_mask(rule_t *curr_rule){
+	return parse_ip(&(curr_rule->dst_prefix_mask));
+}
+
+static int parse_prefix_size(__u8 *dst){
+	memcpy(dst, input_buf_pointer+input_buf_index, sizeof(__u8));
+	return sizeof(__u8); //return the length of the parsed element
+}
+
+static int parse_src_prefix_size(rule_t *curr_rule){
+	return parse_prefix_size(&(curr_rule->src_prefix_size));
+}
+
+static int parse_dst_prefix_size(rule_t *curr_rule){
+	return parse_prefix_size(&(curr_rule->dst_prefix_size));
+}
+
+static int parse_protocol(rule_t *curr_rule){
+	__u8 *dst = &(curr_rule->protocol);
+	switch ((input_buf_pointer+input_buf_index)[0])
 	{
 	case '0':
 		*dst = 255;
@@ -145,55 +134,62 @@ static int parse_protocol(const char *src, __u8 *dst){
 		*dst = 143;
 		break;
 	default:
-		return -1;
+		return -1; //error code
 	}	
-	return 1;
+	return 1; //return the length of the parsed element
 }
 
-static int parse_action(const char *src, __u8 *dst){
-	if (src[0]=='0'){
+static int parse_port(__be16 *dst){
+	memcpy(dst, (input_buf_pointer+input_buf_index), sizeof(__le16));
+	return sizeof(__le16); //return the length of the parsed element
+}
+
+static int parse_src_port(rule_t *curr_rule){
+	return parse_port(&(curr_rule->src_port));
+}
+
+static int parse_dst_port(rule_t *curr_rule){
+	return parse_port(&(curr_rule->dst_port));
+}
+
+static int parse_ack(rule_t *curr_rule){
+	ack_t *dst = &(curr_rule->ack);
+	*dst = (input_buf_pointer+input_buf_index)[0]-'0';
+	if ((0<=*dst)&&(*dst<=3)){
+		return 1; //return the length of the parsed element 
+	}
+	return -1; //error code
+}
+
+static int parse_action(rule_t *curr_rule){
+	__u8 *dst = &(curr_rule->action);
+	if ((input_buf_pointer+input_buf_index)[0]=='0'){
 		*dst=NF_DROP;
 	}
-	else if (src[0]=='1')
+	else if ((input_buf_pointer+input_buf_index)[0]=='1')
 	{
 		*dst=NF_ACCEPT;
 	}
 	else{
-		return -1;
+		return -1; //error code
 	}
-	return 1;
+	return 1; //return the length of the parsed element
 	
 }
 
-static int parse_port(const char *src, __be16 *dst){
-	unsigned long src_int;
-	int size = 0;
-	char *short_src;
-	while (src[size]!=' '){
-		size++;
+//parse the whole rule- call every other parsing function on the rule
+int parse_rule(ParseFieldFuncPointer funcs[], rule_t *curr_rule){
+	int element_size;
+	int func_idx;
+	for (func_idx=0; func_idx< RULE_FILEDS; func_idx++){
+		element_size = funcs[func_idx](curr_rule);
+		if (element_size==-1){
+			return -1;
+		}
+		else{
+			input_buf_index += element_size+1;
+		}
 	}
-	short_src = (char*)kmalloc(size*sizeof(char), GFP_KERNEL);
-	if (!short_src){
-		return -1;
-	}
-	strncpy(short_src, src, size);
-	short_src[size]='\0';
-	if ((kstrtoul(short_src, 10, &src_int) != 0)||((src_int<0)||(src_int>1023))){
-		kfree(short_src);
-		return -1;
-	}
-	kfree(short_src);
-	*dst = htons((__be16)src_int);
-	return size;
-}
-
-
-//Update the current index in buf by the size of element read
-int check_and_update_idx(int *buf_index, int element_size){
-	if (element_size==-1){
-		return -1; //error code
-	}
-	*buf_index += element_size+1;
 	return 0;
 }
 
@@ -202,49 +198,15 @@ int check_and_update_idx(int *buf_index, int element_size){
 ssize_t modify(struct device *dev, struct device_attribute *attr, const char *buf, size_t count)
 {
 	int rule_table_index = 0;
-	int buf_index = 0;
-	while (buf_index<count)
+	input_buf_index = 0;
+	input_buf_pointer = buf;
+	while (input_buf_index<count)
 	{
 		rule_t *curr_rule = rule_table+rule_table_index;
-		buf_index += parse_rule_name(buf+buf_index, 
-		rule_table[rule_table_index].rule_name)+1;
-		if(check_and_update_idx(&buf_index, parse_direction(
-		buf+buf_index, &curr_rule->direction))==-1){
-			return -1;
-		}
-		if(check_and_update_idx(&buf_index, parse_ip(buf+buf_index, &curr_rule->src_ip))==-1){
-			return -1;
-		}
-		if(check_and_update_idx(&buf_index, parse_ip(buf+buf_index, &curr_rule->src_prefix_mask))==-1){
-			return -1;
-		}
-		if(check_and_update_idx(&buf_index, parse_perfix_size(buf+buf_index, &curr_rule->src_prefix_size))==-1){
-			return -1;
-		}
-		if(check_and_update_idx(&buf_index, parse_ip(buf+buf_index, &curr_rule->dst_ip))==-1){
-			return -1;
-		}
-		if(check_and_update_idx(&buf_index, parse_ip(buf+buf_index, &curr_rule->dst_prefix_mask))==-1){
-			return -1;
-		}
-		if(check_and_update_idx(&buf_index, parse_perfix_size(buf+buf_index, &curr_rule->dst_prefix_size))==-1){
-			return -1;
-		}
-		if(check_and_update_idx(&buf_index, parse_protocol(buf+buf_index, &curr_rule->protocol))==-1){
-			return -1;
-		}
-		if(check_and_update_idx(&buf_index, parse_port(buf+buf_index, &curr_rule->src_port))==-1){
-			return -1;
-		}
-		if(check_and_update_idx(&buf_index, parse_port(buf+buf_index, &curr_rule->dst_port))==-1){
-			return -1;
-		}
-		if(check_and_update_idx(&buf_index, parse_ack(buf+buf_index, &curr_rule->ack))==-1){
-			return -1;
-		}
-		if(check_and_update_idx(&buf_index, parse_action(buf+buf_index, &curr_rule->action))==-1){
-			return -1;
-		}
+		ParseFieldFuncPointer parse_funcs[] = {parse_rule_name, parse_direction, parse_src_ip, parse_src_prefix_mask, \
+		parse_src_prefix_size, parse_dst_ip, parse_dst_prefix_mask, parse_dst_prefix_size, parse_protocol, parse_src_port, \
+		parse_dst_port, parse_ack, parse_action};
+		parse_rule(parse_funcs, curr_rule);
 		rule_table_index++;
 	}
 	*rule_table_size = rule_table_index;
--- rules_functions.py
+++ rules_functions.py
@@ -20,125 +20,134 @@ def is_ip(check_ip):
         return True
     else:
         return False
+    
+def parse_rule_name(rule_name):
+    if len(rule_name)>20:
+        return False
+    rule_name = rule_name.ljust(21, '\0')
+    return struct.pack("<21s", rule_name.encode())
 
+def reverse_parse_rule_name(rule_name):
+    return struct.unpack('<21s', rule_name)[0].decode().rstrip('\0')
 
 def parse_ip(ip_add):
     if ip_add == "any":
-        return "10.1.1.1", "255.0.0.0", "8"
-    if not is_ip(ip_add):
+        ip = "10.1.1.1"
+        prefix_size = "8"
+    elif not is_ip(ip_add):
         return False, False, False
-    ip, prefix_size = ip_add.split("/")
+    else:
+        ip, prefix_size = ip_add.split("/")
     prefix_mask = socket.inet_ntoa(struct.pack(">L", (1 << 32) - (1 << 32 >> int(prefix_size))))
-    return ip, prefix_mask, prefix_size
+    ip = b"".join([int(x).to_bytes(1, 'little') for x in ip.split(".")])
+    prefix_mask = b"".join([int(x).to_bytes(1, 'little') for x in prefix_mask.split(".")])
+    return ip, prefix_mask, struct.pack("<B", int(prefix_size))
 
 
 def reverse_parse_ip(ip, prefix_size):
+    perfix_size= str(struct.unpack("<B", perfix_size))
+    ip = [str(int(byte)) for byte in ip]
     result = "/".join([ip, prefix_size])
     if result==r"10.0.0.1/8": # this is the meaning of any ip
-        return any
+        return "any"
     return result
 
 
 def get_direction_code(direction):
     if direction == "in":
-        return str(1)
+        return b'\x01'
     elif direction == "out":
-        return str(2)
+        return b'\x02'
     elif direction == "any":
-        return str(3)
+        return b'\x03'
     else:
         return False  # error code
 
 
 def reverse_direction(direction_code):
-    if direction_code == str(1):
+    if direction_code == b'1':
         return "in"
-    elif direction_code == str(2):
+    elif direction_code == b'2':
         return "out"
-    elif direction_code == str(3):
+    elif direction_code == b'3':
         return "any"
 
 
 def get_protocol_code(protocol):
-    protocol_num = 0 #255
+    protocol_num = b'0' #255
     if protocol == "ICMP":
-        protocol_num = 1 #1
+        protocol_num = b'1' #1
     elif protocol == "TCP":
-        protocol_num = 2 #6
+        protocol_num = b'2' #6
     elif protocol == "UDP":
-        protocol_num = 3 #17
+        protocol_num = b'3' #17
     elif protocol == "any":
-        protocol_num = 4 #143
-    return str(protocol_num)
+        protocol_num = b'4' #143
+    return protocol_num
 
 
 def reverse_protocol(protocol_code):
-    if protocol_code == str(1):
+    if protocol_code == b'1':
         return "ICMP"
-    elif protocol_code == str(2):
+    elif protocol_code == b'2':
         return "TCP"
-    elif protocol_code == str(3):
+    elif protocol_code == b'3':
         return "UDP"
-    elif protocol_code == str(4):
+    elif protocol_code == b'4':
         return "any"
-    elif protocol_code == str(0):
+    elif protocol_code == b'0':
         return "OTHER"
 
 
 def get_port_code(port):
     if port == ">1023":
-        return str(1023)
+        port = 1023
     elif port == "any":
-        return str(0)
-    elif is_int(port) and 1 <= int(port) and int(port) <= 1023:
-        return port
-    else:
-        return False  # error code
+        port =0
+    elif not (is_int(port) and 1 <= int(port) and int(port) <= 1023):
+        return False
+    return struct.pack("<H", int(port))
 
 
 def reverse_port(port_code):
-    if port_code == str(1023):
-        return ">1023"
-    elif port_code == str(0):
-        return "any"
-    else:
-        return port_code
+    return str(struct.unpack("<H", port_code)[0])
 
 
 def get_ack_code(ack):
     if ack == "yes":
-        return str(1)
+        return b'1'
     elif ack == "no":
-        return str(2)
+        return b'2'
     elif ack == "any":
-        return str(3)
+        return b'3'
     else:
         return False  # error code
 
 
 def reverse_ack(ack_code):
-    if ack_code == str(1):
+    if ack_code == b'1':
         return "yes"
-    elif ack_code == str(2):
+    elif ack_code == b'2':
         return "no"
-    elif ack_code == str(3):
+    elif ack_code == b'3':
         return "any"
 
 def get_action_code(action):
     if action=="accept":
-        return str(1)
+        return b'1'
     elif action=="drop":
-        return str(0)
+        return b'0'
     else:
         return False
     
 def reverse_action(action_code):
-    if action_code=="1":
+    if action_code==b'1':
         return "accept"
-    elif action_code=="0":
+    elif action_code==b'0':
         return "drop"
 
 def read_rule(rule):
+    rule_name = parse_rule_name(rule[0])
     direction = get_direction_code(rule[1])
     src_ip, src_prefix_mask, src_prefix_size = parse_ip(rule[2])
     dst_ip, dst_prefix_mask, dst_prefix_size = parse_ip(rule[3])
@@ -147,8 +156,8 @@ def read_rule(rule):
     dst_port = get_port_code(rule[6])
     ack = get_ack_code(rule[7])
     action = get_action_code(rule[8])
-    if src_ip and dst_ip and src_port and dst_port and ack and action:
-        return ' '.join([rule[0], direction, src_ip, src_prefix_mask, src_prefix_size, dst_ip, dst_prefix_mask,
+    if rule_name and src_ip and dst_ip and src_port and dst_port and ack and action:
+        return b' '.join([rule_name, direction, src_ip, src_prefix_mask, src_prefix_size, dst_ip, dst_prefix_mask,
                          dst_prefix_size, protocol, src_port, dst_port, ack, action])
     else:
         return False
@@ -169,7 +178,7 @@ def write_rule(rule):
 def load(rules_file_path):
     try:
         with open(rules_file_path, "r") as rules_file:
-            with open(RULES_DEVICE_FILEPATH, "w") as rules_table_driver:
+            with open(RULES_DEVICE_FILEPATH, "wb") as rules_table_driver:
                 rule = rules_file.readline()
                 while True:
                     if rule=='':
@@ -179,7 +188,7 @@ def load(rules_file_path):
                     parsed_rule = read_rule(rule)
                     if not parsed_rule:
                         return False
-                    rules_table_driver.write(parsed_rule+"\n") # added line terminator to identify end of rule
+                    rules_table_driver.write(parsed_rule+b"\n") # added line terminator to identify end of rule
                     rule = rules_file.readline()
         return True
     except Exception as e:
@@ -188,12 +197,12 @@ def load(rules_file_path):
 
 def show():
     try:
-        with open(RULES_DEVICE_FILEPATH, "r") as rules_file:
+        with open(RULES_DEVICE_FILEPATH, "rb") as rules_file:
             rule = rules_file.readline()
             while True:
-                if rule=='\n':
+                if rule==b'\n':
                     break
-                rule = rule.split()
+                rule = rule.split(b' ')
                 print(write_rule(rule))
                 rule = rules_file.readline()
         return True
